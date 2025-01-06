1d5bd4d2812759748336a98d43c5d92e2c3bc23d
Rabinder Singh <rab_singh@hotmail.com>
2025-01-06 11:27:56 +0000

    edit and delete removed form views

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -149,4 +149,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'item_list'
-LOGIN_URL = 'login'
+LOGIN_URL = '/login/'
--- shopping_list_app/urls.py
+++ shopping_list_app/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('add/', views.add_item, name='add_item'),
     path('', views.item_list, name='item_list'),
+    path('register/', views.register, name='register'), 
 ]
