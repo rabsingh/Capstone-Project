b3c24b715fd04cbebc2b16c01c272bab220d6195
Rabinder Singh <rab_singh@hotmail.com>
2025-01-06 10:10:18 +0000

    register url corrected

--- shopping_list_app/views.py
+++ shopping_list_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ItemForm
 from .models import Item
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 @login_required
 def add_item(request):
@@ -19,3 +21,15 @@ def add_item(request):
 def item_list(request):
     items = Item.objects.all().order_by('-date_added')
     return render(request, 'shopping_list_app/item_list.html', {'items': items})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully. You can now log in.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
