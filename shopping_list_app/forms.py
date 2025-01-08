e032ef0e3d1f93e571fb21f144479b873c069095
Rabinder Singh <rab_singh@hotmail.com>
2025-01-08 11:19:48 +0000

    tidied up forms.py code laypout

--- shopping_list_app/forms.py
+++ shopping_list_app/forms.py
@@ -2,6 +2,19 @@ from django import forms
 from .models import Item
 from django.contrib.auth.models import User
 
+# Form for adding or editing items
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['item_name', 'quantity', 'notes', 'week_beginning']  # Specify fields
+        widgets = {
+            'item_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
+            'notes': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'week_beginning': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        }
+
+# Filtering by week_beginning, added_by , authorised
 class ItemFilterForm(forms.Form):
     week_beginning = forms.DateField(
         required=False,
@@ -22,17 +35,3 @@ class ItemFilterForm(forms.Form):
         label='Authorization Status'
     )
 
-
-
-
-    # Form for adding or editing items
-class ItemForm(forms.ModelForm):
-    class Meta:
-        model = Item
-        fields = ['item_name', 'quantity', 'notes', 'week_beginning']  # Specify fields
-        widgets = {
-            'item_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
-            'notes': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'week_beginning': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-        }
