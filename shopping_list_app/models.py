81ba87063edf15f536435f5df09e124faccb0f84
Rabinder Singh <rab_singh@hotmail.com>
2025-01-09 10:19:28 +0000

    Only a monday week beginning date can be used.

--- shopping_list_app/models.py
+++ shopping_list_app/models.py
@@ -1,15 +1,24 @@
-from django.db import models
-from django.contrib.auth.models import User  # Import Django's built-in User model
+from django.db import models 
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from datetime import datetime
 
-# Items Table
+def validate_monday(value):
+    if value.weekday() != 0:
+        raise ValidationError('Week beginning date must be a Monday.')
+
 class Item(models.Model):
     item_name = models.CharField(max_length=100)
     quantity = models.PositiveIntegerField(default=1)
     notes = models.TextField(blank=True, null=True)
     authorised = models.BooleanField(default=False)
     added_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="added_items")
-    week_beginning = models.DateField()
+    week_beginning = models.DateField(validators=[validate_monday])
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.item_name} ({'Authorised' if self.authorised else 'Pending'})"
